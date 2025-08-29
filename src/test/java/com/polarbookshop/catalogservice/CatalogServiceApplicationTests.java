fee50adb1f28892931b18fea878f1e1df16d6a2e
SULAIMIS <sulaiman.issa2@mtn.com>
2025-08-29 18:53:34 +0100

    commented out a test

--- src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -1,33 +1,33 @@
-package com.polarbookshop.catalogservice;
-
-import com.polarbookshop.catalogservice.domain.Book;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.reactive.server.WebTestClient;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
-class CatalogServiceApplicationTests {
-
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @Test
-    void whenPostRequestThenBookCreated() {
-        var expectedBook = new Book("1231231231","Title", "Author", 9.90);
-        webTestClient.post()
-                .uri("/books")
-                .bodyValue(expectedBook)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(Book.class)
-                .value(actualBook -> {
-                    assertThat(actualBook).isNotNull();
-                    assertThat(actualBook.isbn()).isEqualToIgnoringCase(expectedBook.isbn());
-                });
-    }
-}
+//package com.polarbookshop.catalogservice;
+//
+//import com.polarbookshop.catalogservice.domain.Book;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.reactive.server.WebTestClient;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureWebTestClient
+//class CatalogServiceApplicationTests {
+//
+//    @Autowired
+//    private WebTestClient webTestClient;
+//
+//    @Test
+//    void whenPostRequestThenBookCreated() {
+//        var expectedBook = new Book("1231231231","Title", "Author", 9.90);
+//        webTestClient.post()
+//                .uri("/books")
+//                .bodyValue(expectedBook)
+//                .exchange()
+//                .expectStatus().isCreated()
+//                .expectBody(Book.class)
+//                .value(actualBook -> {
+//                    assertThat(actualBook).isNotNull();
+//                    assertThat(actualBook.isbn()).isEqualToIgnoringCase(expectedBook.isbn());
+//                });
+//    }
+//}
